35668f00cd1765d06b14c8dfad2607188a46e6cc
Dmitry M. Gavrovsky <dmitrygavrovsky@gmail.com>
2024-05-10 15:35:09 +0300

    fix: ignore trial product price during checkout

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,43 +14,43 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 local map = vim.keymap.set
 
--- map("n", "q", function()
---   local plugins = {
---     "blame",
---     "checkhealth",
---     "fugitive",
---     "fugitiveblame",
---     "help",
---     "httpResult",
---     "lspinfo",
---     "notify",
---     "PlenaryTestPopup",
---     "qf",
---     "query",
---     "spectre_panel",
---     "startuptime",
---     "tsplayground",
---   }
---   local buf = vim.api.nvim_get_current_buf()
---   if vim.tbl_contains(plugins, vim.bo[buf].filetype) then
---     vim.bo[buf].buflisted = false
---     vim.api.nvim_win_close(0, false)
---   else
---     -- Find non-floating windows
---     local wins = vim.fn.filter(vim.api.nvim_list_wins(), function(_, win)
---       if vim.api.nvim_win_get_config(win).zindex then
---         return nil
---       end
---       return win
---     end)
---     -- If last window, quit
---     if #wins > 1 then
---       vim.api.nvim_win_close(0, false)
---     else
---       vim.cmd([[quit]])
---     end
---   end
--- end, { desc = "Close window" })
+map("n", "q", function()
+  local plugins = {
+    "blame",
+    "checkhealth",
+    "fugitive",
+    "fugitiveblame",
+    "help",
+    "httpResult",
+    "lspinfo",
+    "notify",
+    "PlenaryTestPopup",
+    "qf",
+    "query",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+  }
+  local buf = vim.api.nvim_get_current_buf()
+  if vim.tbl_contains(plugins, vim.bo[buf].filetype) then
+    vim.bo[buf].buflisted = false
+    vim.api.nvim_win_close(0, false)
+  else
+    -- Find non-floating windows
+    local wins = vim.fn.filter(vim.api.nvim_list_wins(), function(_, win)
+      if vim.api.nvim_win_get_config(win).zindex then
+        return nil
+      end
+      return win
+    end)
+    -- If last window, quit
+    if #wins > 1 then
+      vim.api.nvim_win_close(0, false)
+    else
+      vim.cmd([[quit]])
+    end
+  end
+end, { desc = "Close window" })
 
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 

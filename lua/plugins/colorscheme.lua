d44db2ccc3caf6066565033fa454e1a87c1939ca
Dmitry M. Gavrovsky <dmitrygavrovsky@gmail.com>
2025-12-01 19:06:25 +0200

    feat: new themes"

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -45,10 +45,55 @@ return {
     end,
   },
 
+  -- Zenbones colorscheme collection
+  {
+    "zenbones-theme/zenbones.nvim",
+    lazy = false,
+    priority = 1000,
+    dependencies = { "rktjmp/lush.nvim" },
+    config = function()
+      -- Set to dark background (change to "light" for light mode)
+      vim.o.background = "dark"
+
+      -- Zenbones configuration
+      -- Using "stark" for darker background, increased lightening for brighter colors
+      vim.g.zenbones = {
+        darkness = "stark", -- Darker background
+        lighten_comments = 55, -- Brighter comments
+        lighten_line_nr = 50, -- Brighter line numbers
+        italic_comments = true,
+        italic_strings = false,
+        solid_line_nr = false,
+        colorize_diagnostic_underline_text = true,
+      }
+
+      -- Apply same config to kanagawabones and seoulbones
+      vim.g.kanagawabones = {
+        darkness = "stark",
+        lighten_comments = 55,
+        lighten_line_nr = 50,
+        italic_comments = true,
+        italic_strings = false,
+        colorize_diagnostic_underline_text = true,
+      }
+
+      vim.g.seoulbones = {
+        darkness = "stark",
+        lighten_comments = 55,
+        lighten_line_nr = 50,
+        italic_comments = true,
+        italic_strings = false,
+        colorize_diagnostic_underline_text = true,
+      }
+    end,
+  },
+
   -- Override LazyVim's default colorscheme setting
   {
     "LazyVim/LazyVim",
     opts = {
+      -- Options: "vague", "kanagawa-paper", "zenbones", "zenwritten",
+      -- "kanagawabones", "rosebones", "forestbones", "seoulbones"
       colorscheme = "vague",
     },
   },
